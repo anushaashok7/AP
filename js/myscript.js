70e1e8e7ac524ccfe6b04d1aa76f669fde98e819
Anusha <aa2004@cam.ac.uk>
2018-03-06 18:54:34 +0000

    javascript sorted

--- js/myscript.js
+++ js/myscript.js
@@ -1,41 +1,56 @@
-function() {
+console.log("test");
 
-  'use strict';
+//'use strict';
+// define variables
+var items = [];
 
-  // define variables
-  var items = document.querySelectorAll(".timeline li");
+function populateItems() {
+  items = document.querySelectorAll(".timeline li");  
+}
 
-  // check if an element is in viewport
-  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
-  function isElementInViewport(el) {
-    var rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
+// check if an element is in viewport
+// http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
 
-  function callbackFunc() {
-    for (var i = 0; i < items.length; i++) {
-      if (isElementInViewport(items[i])) {
-        items[i].classList.add("in-view");
-      }
+function callbackFunc() {
+  console.log('came here 3.5');
+  console.log(items.length);
+
+  for (var i = 0; i < items.length; i++) {
+    console.log('came here 4 ' + i);
+
+    if (isElementInViewport(items[i])) {
+      console.log('came here 5 ' +i);
+
+      items[i].classList.add("in-view");
     }
   }
+}
 
-  // listen for events
-  window.addEventListener("load", callbackFunc);
-  window.addEventListener("resize", callbackFunc);
-  window.addEventListener("scroll", callbackFunc);
+// listen for events
+window.addEventListener("load", populateItems);
+console.log('came here 0');
+window.addEventListener("load", callbackFunc);
+console.log('came here 1');
+window.addEventListener("resize", callbackFunc);
+console.log('came here 2');
+window.addEventListener("scroll", callbackFunc);
+console.log('came here 3');
 
-})();
 /* SCROLL BUTTON
-–––––––––––––––––––––––––––––––––––––––––––––––––– */
+––––––––––––––––––––––––––––––––––––––––––––––––––
 $(function() {
   $('a[href*=#]').on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
   });
 });
+*/
